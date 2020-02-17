376a8d64ec82d8e9ea7a78806cf22c94a281cf12
Van <vango85@gmail.com>
2020-02-17 11:05:33 +0100

    [xx] End of live session

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
-const container = document.getElementById("main");
-fetch('http://api.open-notify.org/astros.json')
+/*
+Tasks:
+ 1. * Add total number of launches
+ 2. Add launch number, mission name and date to HTML
+ 3. Change launch date to show different days of the week, locales
+ 4. toLocaleDateString() vs. custom array 
+*/
+
+const container = document.getElementById("main"); // div
+const launchCountHeader = document.getElementById("launchCount"); // H1
+const apiUrl = 'https://api.spacexdata.com/v3/launches/past';
+fetch(apiUrl)
     .then((response) => {
         return response.json();
     })
-    .then((json) =>{
-        console.log(json);
-        json.people.forEach(element => {
-            container.innerHTML += `<p>${element.name}</p>`
+    .then((launchArray) => {
+        const numLaunches = launchArray.length;
+        launchCountHeader.innerHTML = `Total Launches: ${numLaunches}`
+        // Do stuff with the JSON here, modify DOM etc..
+        launchArray.forEach((launch) => {
+            const launchDate = new Date(launch.launch_date_utc);
+            console.log(launch);
+
+            // Here we'll make the date readable
+            const launchDateString = launchDate.toLocaleDateString('en-US', {weekday: 'short'});
+            const launchTimeString = launchDate.toLocaleTimeString('en-US', {timestyle: 'short',hour12:false})
+
+            container.innerHTML += `<p>${launch.flight_number}: ${launch.mission_name} - ${launchDateString}, ${launchTimeString}</p><img src='http://placekitten.com/200/300?q=${Math.random()}'></img>`
+
         });
     })
