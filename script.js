4cbcf55a4625abbef0d273640d44d64941e6f4e5
Van <vango85@gmail.com>
2020-09-20 20:53:13 +0200

    [xx] Fall2020 Goal

--- script.js
+++ script.js
@@ -1,31 +1,38 @@
-/*
-Tasks:
- 1. * Add total number of launches
- 2. Add launch number, mission name and date to HTML
- 3. Change launch date to show different days of the week, locales
- 4. toLocaleDateString() vs. custom array 
-*/
+const container = document.getElementById("main"); // main
+const launchCountHeader = document.getElementById("launchCount"); // h1
+const apiUrl = "https://api.spacexdata.com/v3/launches/past";
+launchCountHeader.innerHTML = 15;
 
-const container = document.getElementById("main"); // div
-const launchCountHeader = document.getElementById("launchCount"); // H1
-const apiUrl = 'https://api.spacexdata.com/v3/launches/past';
+const generateHTMLForLaunch = (launch) => {
+  const launchDate = new Date(launch.launch_date_utc);
+  // Here we'll make the date readable
+  const launchDateString = launchDate.toLocaleDateString("en-US", {});
+  const launchTimeString = launchDate.toLocaleTimeString("en-US", {
+    timestyle: "short",
+    hour12: false,
+  });
+
+  console.log(launch.launch_success);
+  const launchOutcomeImage = launch.launch_success
+    ? "success.png"
+    : "failure.png";
+  console.log(launchOutcomeImage);
+
+  let launchHTML = "";
+  launchHTML += `<section class='launch'>`;
+  launchHTML += ` <img src='./${launchOutcomeImage}'>`;
+  launchHTML += ` <p>${launch.flight_number}: ${launch.mission_name} - ${launchDateString}, ${launchTimeString}</p>`;
+  launchHTML += `</section>`;
+  return launchHTML;
+};
+
 fetch(apiUrl)
-    .then((response) => {
-        return response.json();
-    })
-    .then((launchArray) => {
-        const numLaunches = launchArray.length;
-        launchCountHeader.innerHTML = `Total Launches: ${numLaunches}`
-        // Do stuff with the JSON here, modify DOM etc..
-        launchArray.forEach((launch) => {
-            const launchDate = new Date(launch.launch_date_utc);
-            console.log(launch);
-
-            // Here we'll make the date readable
-            const launchDateString = launchDate.toLocaleDateString('en-US', {weekday: 'short'});
-            const launchTimeString = launchDate.toLocaleTimeString('en-US', {timestyle: 'short',hour12:false})
-
-            container.innerHTML += `<p>${launch.flight_number}: ${launch.mission_name} - ${launchDateString}, ${launchTimeString}</p><img src='http://placekitten.com/200/300?q=${Math.random()}'></img>`
-
-        });
-    })
+  .then((response) => {
+    return response.json();
+  })
+  .then((launches) => {
+    launchCountHeader.innerHTML = launches.length;
+    launches.forEach((launch) => {
+      container.innerHTML += generateHTMLForLaunch(launch);
+    });
+  });
